c46a1c79be54612dc31e4fae2b0b22daec7a85b4
Alvaro Estévez <alvaro5estevez@gmail.com>
2023-11-17 15:33:40 +0000

    Appointments endpoints with basic crud

--- api/routers/index.js
+++ api/routers/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 
 router.use('/employees', require('./employee.router.js'))
 router.use('/customers', require('./customer.router.js'))
+router.use('/appointments', require('./appointment.router.js'))
 
 module.exports = router
